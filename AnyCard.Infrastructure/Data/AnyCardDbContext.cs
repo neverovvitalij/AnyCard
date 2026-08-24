a18362d27b1831c9da454bde97f841b872415d1f
Vitalij Neverov <Vitalij.Neverov@training.lug-ag.de>
2026-08-24 16:04:28 +0200

    feat: add AnyCardDbContext with unique index on CardProgress

--- AnyCard.Infrastructure/Data/AnyCardDbContext.cs
+++ AnyCard.Infrastructure/Data/AnyCardDbContext.cs
@@ -5,4 +5,16 @@ namespace AnyCard.Infrastructure.Data;
 public class AnyCardDbContext : DbContext
 {
     public DbSet<Card> Cards { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<CardProgress> CardProgresses { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public AnyCardDbContext(DbContextOptions<AnyCardDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CardProgress>()
+            .HasIndex(cp => new { cp.CardId, cp.UserId })
+            .IsUnique();
+    }
 }
