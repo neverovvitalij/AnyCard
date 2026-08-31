a133c607e9b3aac96679f8afff065e80019ba23f
Vitalij Neverov <Vitalij.Neverov@training.lug-ag.de>
2026-08-31 15:46:56 +0200

    feat: implement spaced repetition interval calculator

---
+++ AnyCard.Application/Services/ISpacedRepetitionCalculator.cs
@@ -0,0 +1,17 @@
+﻿
+using AnyCard.Domain.Enums;
+
+namespace AnyCard.Application.Services;
+public class SpacedRepetitionCalculator
+{
+    private const double GrowthFactor = 2.0;
+    public static TimeSpan CalculateNextReview(UserRating userRating, int currentViewCounter) => userRating switch
+    {
+        UserRating.Again => TimeSpan.FromMinutes(20),
+        UserRating.Hard => TimeSpan.FromHours(2) * Math.Pow(GrowthFactor, currentViewCounter),
+        UserRating.Good => TimeSpan.FromDays(3) * Math.Pow(GrowthFactor, currentViewCounter),
+        UserRating.Easy => TimeSpan.FromDays(7) * Math.Pow(GrowthFactor, currentViewCounter),
+        _ => throw new ArgumentOutOfRangeException()
+    };
+    
+}
