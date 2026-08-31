a504172690a7f5a780b9e2a6b1885e541eb71138
Vitalij Neverov <Vitalij.Neverov@training.lug-ag.de>
2026-08-31 13:14:28 +0200

    feat: add CardRepository

--- AnyCard.Application/Interfaces/ICardRepository.cs
+++ AnyCard.Application/Interfaces/ICardRepository.cs
@@ -1,5 +1,13 @@
 ﻿
+using AnyCard.Domain.Model;
+
 namespace AnyCard.Application.Interfaces;
 public interface ICardRepository
 {
+    Task AddAsync(Card card);
+    Task<List<Card>> GetAllAsync();
+    Task<Card?> GetByIdAsync(int id);
+    Task<bool> SaveChangesAsync();
+    void Delete(Card card);
+
 }
--- AnyCard.Infrastructure/Repositories/CardRepository.cs
+++ AnyCard.Infrastructure/Repositories/CardRepository.cs
@@ -1,5 +1,36 @@
 ﻿
+using AnyCard.Application.Interfaces;
+using AnyCard.Domain.Model;
+using AnyCard.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace AnyCard.Infrastructure.Repositories;
-public class CardRepository
+public class CardRepository : ICardRepository
 {
+    private readonly AnyCardDbContext _anyCardDbContext;
+    public CardRepository(AnyCardDbContext anyCardDbContext)
+    {
+        _anyCardDbContext = anyCardDbContext;
+    }
+
+    public async Task AddAsync(Card card)
+    {
+        await _anyCardDbContext.Cards.AddAsync(card);
+    }
+    public async Task<List<Card>> GetAllAsync()
+    {
+        return await _anyCardDbContext.Cards.ToListAsync();
+    }
+    public async Task<Card?> GetByIdAsync(int id)
+    {
+        return await _anyCardDbContext.Cards.FindAsync(id);
+    }
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _anyCardDbContext.SaveChangesAsync() > 0;
+    }
+    public void Delete(Card card)
+    {
+        _anyCardDbContext.Cards.Remove(card);
+    }
 }
