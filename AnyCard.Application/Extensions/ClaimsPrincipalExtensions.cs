081cd8dbb2856930d867d8930aef3ba8513ffabd
Vitalij Neverov <Vitalij.Neverov@training.lug-ag.de>
2026-09-08 15:45:31 +0200

    feat: add ClaimsPrincipal extension for retrieving current user id

---
+++ AnyCard.Application/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Security.Claims;
+
+namespace AnyCard.Application.Extensions;
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return int.Parse(userIdClaim!);
+    }
+}
---
+++ AnyCard/Controllers/CardsController.cs
@@ -0,0 +1,19 @@
+﻿using AnyCard.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using AnyCard.Application.Extensions;
+
+namespace AnyCard.Controllers;
+
+public class CardsController : ControllerBase
+{
+    private readonly ICardRepository _cardRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CardsController(ICategoryRepository categoryRepository, ICardRepository cardRepository)
+    {
+        _cardRepository = cardRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+
+}
